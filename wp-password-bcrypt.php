842ed575f3c8000fddd5041d31310cb57b4a9b7a
Daisuke Takahashi <daisuke@extendwings.com>
2016-03-04 09:42:41 +0900

    Follow WordPress PHP Coding Standards: "Naming Conventions"

--- wp-password-bcrypt.php
+++ wp-password-bcrypt.php
@@ -16,12 +16,12 @@ const WP_OLD_HASH_PREFIX = '$P$';
  *
  * @param string $password Plaintext password
  * @param string $hash Hash of password
- * @param int|string $userId ID of user to whom password belongs
+ * @param int|string $user_id ID of user to whom password belongs
  * @return mixed|void
  *
  * @SuppressWarnings(PHPMD.CamelCaseVariableName) $wp_hasher is a global variable, we cannot change its name
  */
-function wp_check_password( $password, $hash, $userId = '' ) {
+function wp_check_password( $password, $hash, $user_id = '' ) {
 	if ( strpos( $hash, WP_OLD_HASH_PREFIX ) === 0 ) {
 		global $wp_hasher;
 
@@ -33,13 +33,13 @@ function wp_check_password( $password, $hash, $userId = '' ) {
 
 		$check = $wp_hasher->CheckPassword( $password, $hash );
 
-		if ( $check && $userId ) {
-			$hash = wp_set_password( $password, $userId );
+		if ( $check && $user_id ) {
+			$hash = wp_set_password( $password, $user_id );
 		}
 	}
 
 	$check = password_verify( $password, $hash );
-	return apply_filters( 'check_password', $check, $password, $hash, $userId );
+	return apply_filters( 'check_password', $check, $password, $hash, $user_id );
 }
 
 /**
@@ -57,17 +57,17 @@ function wp_hash_password( $password ) {
  * Set password using bcrypt
  *
  * @param string $password Plaintext password
- * @param int $userId ID of user to whom password belongs
+ * @param int $user_id ID of user to whom password belongs
  * @return bool|string
  */
-function wp_set_password( $password, $userId ) {
+function wp_set_password( $password, $user_id ) {
 	/** @var \wpdb $wpdb */
 	global $wpdb;
 
 	$hash = wp_hash_password( $password );
 
-	$wpdb->update( $wpdb->users, [ 'user_pass' => $hash, 'user_activation_key' => '' ], [ 'ID' => $userId ] );
-	wp_cache_delete( $userId, 'users' );
+	$wpdb->update( $wpdb->users, [ 'user_pass' => $hash, 'user_activation_key' => '' ], [ 'ID' => $user_id ] );
+	wp_cache_delete( $user_id, 'users' );
 
 	return $hash;
 }

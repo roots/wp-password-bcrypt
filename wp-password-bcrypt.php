ccfc0f62db12bcd3cb29b75965ce1f293961356f
Daisuke Takahashi <daisuke@extendwings.com>
2016-03-04 09:38:02 +0900

    Follow WordPress PHP Coding Standards: "Space Usage"

--- wp-password-bcrypt.php
+++ wp-password-bcrypt.php
@@ -21,26 +21,25 @@ const WP_OLD_HASH_PREFIX = '$P$';
  *
  * @SuppressWarnings(PHPMD.CamelCaseVariableName) $wp_hasher is a global variable, we cannot change its name
  */
-function wp_check_password($password, $hash, $userId = '')
-{
-    if (strpos($hash, WP_OLD_HASH_PREFIX) === 0) {
+function wp_check_password( $password, $hash, $userId = '' ) {
+    if ( strpos( $hash, WP_OLD_HASH_PREFIX ) === 0 ) {
         global $wp_hasher;
 
-        if (empty($wp_hasher)) {
-            require_once(ABSPATH . WPINC . '/class-phpass.php');
+        if ( empty( $wp_hasher ) ) {
+            require_once( ABSPATH . WPINC . '/class-phpass.php' );
 
-            $wp_hasher = new PasswordHash(8, true);
+            $wp_hasher = new PasswordHash( 8, true );
         }
 
-        $check = $wp_hasher->CheckPassword($password, $hash);
+        $check = $wp_hasher->CheckPassword( $password, $hash );
 
-        if ($check && $userId) {
-            $hash = wp_set_password($password, $userId);
+        if ( $check && $userId ) {
+            $hash = wp_set_password( $password, $userId );
         }
     }
 
-    $check = password_verify($password, $hash);
-    return apply_filters('check_password', $check, $password, $hash, $userId);
+    $check = password_verify( $password, $hash );
+    return apply_filters( 'check_password', $check, $password, $hash, $userId );
 }
 
 /**
@@ -49,10 +48,9 @@ function wp_check_password($password, $hash, $userId = '')
  * @param string $password Plaintext password
  * @return bool|string
  */
-function wp_hash_password($password)
-{
-    $options = apply_filters('wp_hash_password_options', []);
-    return password_hash($password, PASSWORD_DEFAULT, $options);
+function wp_hash_password( $password ) {
+    $options = apply_filters( 'wp_hash_password_options', [] );
+    return password_hash( $password, PASSWORD_DEFAULT, $options );
 }
 
 /**
@@ -62,15 +60,14 @@ function wp_hash_password($password)
  * @param int $userId ID of user to whom password belongs
  * @return bool|string
  */
-function wp_set_password($password, $userId)
-{
+function wp_set_password( $password, $userId ) {
     /** @var \wpdb $wpdb */
     global $wpdb;
 
-    $hash = wp_hash_password($password);
+    $hash = wp_hash_password( $password );
 
-    $wpdb->update($wpdb->users, ['user_pass' => $hash, 'user_activation_key' => ''], ['ID' => $userId]);
-    wp_cache_delete($userId, 'users');
+    $wpdb->update( $wpdb->users, [ 'user_pass' => $hash, 'user_activation_key' => '' ], [ 'ID' => $userId ] );
+    wp_cache_delete( $userId, 'users' );
 
     return $hash;
 }
